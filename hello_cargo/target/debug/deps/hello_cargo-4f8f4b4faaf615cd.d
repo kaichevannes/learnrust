8433f1a27b204701e75d5a5df10646feb4c80db2
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-22 18:24:14 +0100

    chore: remove build fragments in source control

--- hello_cargo/target/debug/deps/hello_cargo-4f8f4b4faaf615cd.d
+++
@@ -1,5 +0,0 @@
-/home/focus/learnrust/hello_cargo/target/debug/deps/libhello_cargo-4f8f4b4faaf615cd.rmeta: src/main.rs
-
-/home/focus/learnrust/hello_cargo/target/debug/deps/hello_cargo-4f8f4b4faaf615cd.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/focus/learnrust/hello_cargo/target/debug/hello_cargo: /home/focus/learnrust/hello_cargo/src/main.rs
